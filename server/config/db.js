bee69de23133ad983ef4e7ad1122f4ba3ba66fe3
Arunraj <arunraj072230@gmail.com>
2026-09-23 02:07:12 +0530

    Fix HTTPS backend URL

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const uri = process.env.MONGO_URI;
+
+    console.log("Mongo URI exists:", !!uri);
+    console.log("Mongo URI starts with:", uri?.substring(0, 20));
+    console.log("Mongo username:", uri?.split("://")[1]?.split(":")[0]);
+
+    await mongoose.connect(uri);
+
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
